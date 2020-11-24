b983344bbe16554697b4373d0518ac6c07f0dd77
Rafael J P D <rafael.pezzuto@gmail.com>
2020-11-24 19:12:13 -0300

    Adiciona dependencias e configuração inicial no setup

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+
+requires = [
+    'mysqlclient',
+    'pyramid',
+    'pyramid_chameleon',
+    'pyramid_tm',
+    'sqlalchemy',
+    'waitress',
+    'zope.sqlalchemy',
+]
+
+dev_requires = [
+    'pyramid_debugtoolbar',
+    'pytest',
+    'webtest',
+]
+
+setup(
+    name='api',
+    install_requires=requires,
+    extras_require={
+        'dev': dev_requires,
+    },
+    entry_points={
+        'paste.app_factory': [
+            'main = api:main'
+        ],
+        'console_scripts': [
+            'initialize_db = api.initialize_db:main'
+        ],
+    },
+)

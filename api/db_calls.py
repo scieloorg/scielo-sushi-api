847d53e73a2583e5f8fd1c4ee05cc6b4c181e93c
Rafael J P D <rafael.pezzuto@gmail.com>
2020-11-24 19:12:45 -0300

    Adiciona módulo que faz chamadas a base de dados

---
+++ api/db_calls.py
@@ -0,0 +1,4 @@
+DB_CALL_TR_J1 = 'CALL TR_J1("%s", "%s")'
+DB_CALL_TR_J1_BY_YM = 'CALL TR_J1_BY_YM("%s", "%s")'
+DB_CALL_TR_J1_JOURNAL = 'CALL TR_J1_JOURNAL("%s", "%s", "%s")'
+DB_CALL_TR_J1_JOURNAL_BY_YM = 'CALL TR_J1_JOURNAL_BY_YM("%s", "%s", "%s")'

a9d0f0998f4e5d6494376cf7870af8eef279cf39
Rafael J P Damaceno <rafael.pezzuto@gmail.com>
2021-03-01 22:26:51 -0300

    adiciona um wrapper para montagem de relatórios

--- api/adapter.py
+++ api/adapter.py
@@ -1,6 +1,15 @@
 from datetime import datetime
 
 
+def wrapper_mount_json_for_report(report_id, result_query, attrs):
+    if report_id == 'ir_a1':
+        return mount_json_for_reports_ir_a1(result_query, attrs)
+    if report_id == 'tr_j1':
+        return mount_json_for_reports_tr_j1(result_query, attrs)
+    if report_id == 'tr_j4':
+        return mount_json_for_reports_tr_j4(result_query, attrs)
+
+
 def mount_json_for_reports(result_query):
     json_results = []
 

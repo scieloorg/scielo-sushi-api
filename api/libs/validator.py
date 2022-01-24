4dbf9d8fd236fbf9888eaf4e0f8bde2e03a62f13
Rafael JPD <rafael.pezzuto@gmail.com>
2022-01-24 20:27:33 -0300

    Adiciona método que verifica se ISSN é válido

--- api/libs/validator.py
+++ api/libs/validator.py
@@ -8,3 +8,9 @@ def is_valid_date_range(begin_date, end_date):
     if end_date >= begin_date:
         return True
     return False
+
+
+def is_valid_issn(issn):
+    if re.match(pattern=values.REGEX_ISSN, string=issn):
+        return True
+    return False

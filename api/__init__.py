1d47251d7a2d3890b396cf48809cee94c439a9b6
Rafael JPD <rafael.pezzuto@gmail.com>
2022-01-24 20:14:47 -0300

    Altera nome de variável de ambiente de MARIADB_ para STR_CONNECTION

--- api/__init__.py
+++ api/__init__.py
@@ -5,7 +5,7 @@ from .models import DBSession, Base
 
 
 def main(global_config, **settings):
-    sqlalchemy_url_value = environ.get('MARIADB_STRING_CONNECTION', 'mysql://root:pass@172.17.0.3:3306/matomo')
+    sqlalchemy_url_value = environ.get('STR_CONNECTION', 'mysql://root:pass@172.17.0.3:3306/matomo')
     settings.update({'sqlalchemy.url': sqlalchemy_url_value})
 
     application_url_value = environ.get('APPLICATION_URL', 'http://127.0.0.1:6543')

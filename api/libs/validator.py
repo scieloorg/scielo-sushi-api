88cc74fdb09c2d0ea077149e6d66a39b04149d93
Rafael JPD <rafael.pezzuto@gmail.com>
2022-01-24 20:28:24 -0300

    Adiciona método geral para validar todos os parâmetros

--- api/libs/validator.py
+++ api/libs/validator.py
@@ -48,3 +48,46 @@ def validate_date_format(param_date, name):
         if 'unconverted data' or 'argument of type' or 'parameter date' in e:
             return errors.error_invalid_date_arguments()
 
+
+def validate_parameters(params, expected_params_list=[]):
+    validation_results = {'errors': []}
+
+    for p in expected_params_list:
+        if p not in params:
+            validation_results['errors'].append((p, errors.error_required_filter_missing(p)))
+
+    for p_name, p_value in params.items():
+        if 'date' in p_name:
+            validation_value = validate_date_format(p_value, p_name)
+
+            if isinstance(validation_value, dict):
+                validation_results['errors'].append((p_name, validation_value))
+
+            else:
+                validation_results[p_name] = validation_value
+
+        elif p_name != 'issn':
+          validation_results[p_name] = p_value
+
+    if 'begin_date' in validation_results and 'end_date' in validation_results:
+        if not is_valid_date_range(validation_results['begin_date'], validation_results['end_date']):
+            validation_value = errors.error_invalid_date_arguments()
+
+            if isinstance(validation_value, dict):
+                validation_results['errors'].append(('period', validation_value))
+
+    if 'issn' in params:
+        if not is_valid_issn(params['issn']):
+            validation_value = errors.error_invalid_report_filter_value(params['issn'], severity='error')
+            if isinstance(validation_value, dict):
+                validation_results['errors'].append(('issn', validation_value))
+        else:
+            validation_results['issn'] = params['issn']
+
+    if 'pid' in params:
+        if not is_valid_pid(params['pid']):
+            validation_value = errors.error_invalid_report_filter_value(params['pid'], severity='error')
+            if isinstance(validation_value, dict):
+                validation_results['errors'].append(('pid', validation_value))
+
+    return validation_results

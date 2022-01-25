5f532429d186a46d69c9ac65b4bbc2ea84d923b5
Rafael JPD <rafael.pezzuto@gmail.com>
2022-01-25 19:55:10 -0300

    Adiciona gerador de relatório lr_j1 em formato json

--- api/adapter.py
+++ api/adapter.py
@@ -1,4 +1,5 @@
 from api import utils, values
+from api.libs import cleaner
 from datetime import datetime
 
 
@@ -152,6 +153,95 @@ def _json_tr_j1(result_query_reports_tr_j1, params, exceptions):
                 }
             }
             report_items[r.journalID]['Performance'].append(performance_m)
+
+        json_results['Report_Items'] = [ri for ri in report_items.values() if ri['Title']]
+    return json_results
+
+
+def _json_lr_j1(result_query_reports_lr_j1, params, exceptions):
+    begin_date, ed_discard = cleaner.get_start_and_last_days(params.get('begin_date', ''))
+    bd_discard, end_date = cleaner.get_start_and_last_days(params.get('end_date', ''))
+
+    json_results = {
+        "Report_Header": {
+            "Created": datetime.now().isoformat(),
+            "Created_By": "Scientific Electronic Library Online SUSHI API",
+            "Customer_ID": params.get('customer', ''),
+            "Report_ID": params.get('report_db_params', {}).report_id,
+            "Release": params.get('report_db_params', {}).release,
+            "Report_Name": params.get('report_db_params', {}).name,
+            "Institution_Name": params.get('institution_name', ''),
+            "Institution_ID": [{
+                "Type": "ISNI",
+                "Value": params.get('institution_id', '')
+            }],
+        },
+        "Report_Filters": [{
+            "Name": "Begin_Date",
+            "Value": str(begin_date),
+        }, {
+            "Name": "End_Date",
+            "Value": str(end_date),
+        }],
+        "Report_Attributes": [{
+            "Name": "Attributes_To_Show",
+            "Value": "Data_Type|Access_Method"
+        }],
+        "Exceptions": exceptions,
+        "Report_Items": []
+    }
+
+    report_items = {}
+
+    for r in result_query_reports_lr_j1:
+        key = (r.journalID, r.articlesLanguage)
+        if key not in report_items:
+            report_items[key] = {
+                'Title': r.title,
+                'Item_ID': [],
+                'Platform': params.get('platform', ''),
+                'Publisher': r.publisherName,
+                'Publisher_ID': [],
+                'Article_Language': r.articlesLanguage,
+                'Data_Type': 'Journal',
+                'Section_Type': 'Article',
+                'Access_Type': 'Open Access',
+                'Access_Method': 'Regular',
+                'Performance': []}
+
+            if r.printISSN:
+                report_items[key]['Item_ID'].append({
+                    "Type": 'Print_ISSN',
+                    "Value": r.printISSN
+                })
+
+            if r.onlineISSN:
+                report_items[key]['Item_ID'].append({
+                    "Type": 'Online_ISSN',
+                    "Value": r.onlineISSN
+                })
+
+        if params['granularity'] == 'monthly':
+            begin_date, end_date  = cleaner.get_start_and_last_days(r.yearMonth)
+
+        elif params['granularity'] == 'totals':
+            begin_date, ed_discard  = cleaner.get_start_and_last_days(r.beginDate)
+            bd_discard, end_date  = cleaner.get_start_and_last_days(r.endDate)
+
+        for m in ['Total_Item_Requests', 'Unique_Item_Requests']:
+            metric_name = m[0].lower() + m[1:].replace('_', '')
+
+            performance_m = {
+                'Period': {
+                    'Begin_Date': str(begin_date),
+                    'End_Date': str(end_date)
+                },
+                'Instance': {
+                    'Metric_Type': m,
+                    'Count': str(getattr(r, metric_name))
+                }
+            }
+            report_items[key]['Performance'].append(performance_m)
 
         json_results['Report_Items'] = [ri for ri in report_items.values() if ri['Title']]
     return json_results

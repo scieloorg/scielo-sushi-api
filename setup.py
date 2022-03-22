90f4cbf6c9a4bd5a2c52bfc313fde43bb6770e52
Rafael JPD <rafael.pezzuto@gmail.com>
2022-03-22 19:27:35 -0300

    Atualiza setup.py para 0.9.4

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ dev_requires = [
 
 setup(
     name='scielo-sushiapi',
-    version='0.9.3',
+    version='0.9.4',
     packages=find_packages(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests", "docs"]
     ),
